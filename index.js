41c5d091cd43cf5e5e3c67d3c7e66c5ef1763a4b
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-05-07 21:32:24 +0700

    cau-hinh-env

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-
+require('dotenv').config()
 const route = require('./routes/client/index.route') // cú pháp để import một module trong nodejs, ở đây là file index.route.js trong thư mục routes/client
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.set('views', './views')
 app.set('view engine', 'pug')
