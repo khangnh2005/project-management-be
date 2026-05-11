61378c9112b2255f90345742247e607165ff9bbd
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-05-11 15:59:34 +0700

    lay data ve ra giao dien product admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,16 @@
 // [GET] /admin/products
-module.exports.adminProducts = (req, res) => {
+
+const Product = require('../../models/product.model');
+module.exports.adminProducts = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+    console.log(products);
+
             res.render('admin/pages/products/index.pug', {
                 titlePage: 'Danh sách san pham'
+                ,
+                products : products
             })
         
     }   
