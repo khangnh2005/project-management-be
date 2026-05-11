d40df0b0444b84abdb7937e07a42f9208449a214
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-05-11 17:09:07 +0700

    Tinh nang loc theo trang thai

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,15 +2,49 @@
 
 const Product = require('../../models/product.model');
 module.exports.adminProducts = async (req, res) => {
-    const products = await Product.find({
-        deleted: false
-    });
-    console.log(products);
+    console.log(req.query.status);
+    let filterStatus = [
+    {
+        name : "Tất cả",
+        status : "",
+        class : ""
+    },
+    {
+        name : "Hoạt động",
+        status : "active",
+        class : ""
+    },
+    {
+        name : "Dừng hoạt động",
+        status : "inactive",
+        class : ""
+    },
+    ]   
+    let find = {
+        deleted: false,
+
+    }
+    if(req.query.status){
+        const index = filterStatus.findIndex(item => item.status == req.query.status)
+        filterStatus[index].class = "active"
+    }
+    else {
+        const index = filterStatus.findIndex(item => item.status == "")
+        filterStatus[index].class = "active"
+    }
+
+    if(req.query.status){
+        find.status = req.query.status
+    }
+    // console.log(req.query.status);
+    const products = await Product.find(find);
+     
 
             res.render('admin/pages/products/index.pug', {
                 titlePage: 'Danh sách san pham'
                 ,
-                products : products
+                products : products,
+                filterStatus : filterStatus
             })
         
     }   
