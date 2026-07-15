5ae9e2706ca1763097e7ca82b1dc3af8f6f76cd1
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-07-15 16:41:49 +0700

    Hien thi san pham moi nhat

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,15 +3,25 @@ const Product = require("../../models/product.model")
 const productsHelpers = require("../../helpers/products")
 // [GET] /home
 module.exports.index = async (req, res) => {
-    let find = {
+
+    //Lay ra san pham noi bat
+    const productsFeatured = await Product.find({
         deleted : false,
         featured : "1",
         status : "active"
-    }
-    const products = await Product.find(find)
-    const newProducts = productsHelpers.priceNewProducts(products)
+    }).limit(6)
+    const newProductsFeatured = productsHelpers.priceNewProducts(productsFeatured)
+    
+    //Lay ra san pham moi nhat
+    const productsNew = await Product.find({
+        deleted : false,
+        status : "active"
+    }).sort({position : "desc"}).limit(6)
+    const newProductsNew = productsHelpers.priceNewProducts(productsNew)
+
     res.render('client/pages/home/index.pug' , {
         titlePage : 'Trang Chủ',
-        products : newProducts
+        productsFeatured : newProductsFeatured,
+        productsNew : newProductsNew
     }) 
 }
