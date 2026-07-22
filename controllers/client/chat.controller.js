3cd245c1b09c6c0eece3484763ae74743a8b4d5f
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-07-22 18:14:11 +0700

    Toi uu lai folder socket

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,64 +1,10 @@
 const Chat = require("../../models/chat.model") 
 const User = require("../../models/users.model") 
-
-const uploadToCloudinary = require("../../helpers/uploadToCloudinary")
+const chatSocket = require("../../sockets/clients/chat.socket")
 
 
 module.exports.index = async (req,res)=>{
-    // SocketIO
-    _io.once('connection', (socket) => {
-        const userId = res.locals.user.id
-        const fullName = res.locals.user.fullName
-        console.log('a user connected' , socket.id);
-
-        socket.on("CLIENT_SEND_MESSAGE" , async (data) =>{
-            const content = data.content 
-            
-            // Convert mảng byte thành Buffer
-            const imagesBuffer = [];
-            if(data.images && data.images.length > 0){
-                for (const imageData of data.images) {
-                    const buffer = Buffer.from(imageData); // array of bytes → Buffer
-                    imagesBuffer.push(buffer);
-                }
-            }
-            
-
-            let images = [];
-            for (const img of imagesBuffer) {
-                const link = await uploadToCloudinary(img);
-                images.push(link)
-            }
-            
-
-            // Lưu vào database
-            const chat = new Chat({
-                user_id : userId ,
-                content : content,
-                images : images
-            })
-            await chat.save();   
-            
-            // Broadcast lại cho tất cả client (kể cả người gửi)
-            _io.emit("SERVER_RETURN_MESSAGE",{
-                userId : userId,
-                fullName : fullName,
-                content : content,
-                images : images
-            })
-        })
-        //Typing
-        socket.on("CLIENT_SEND_TYPING" , async (type) =>{
-            socket.broadcast.emit("SERVER_RETURN_TYPING" , {
-                userId : userId,
-                fullName : fullName,
-                type : type
-            }) 
-        })
-        //Typing
-
-    });
-    //End SocketIOs
+    chatSocket(res)
     // lấy data từ database 
     const chats = await Chat.find({deleted : false})
     
